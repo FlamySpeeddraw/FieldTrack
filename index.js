a76478e0adb48ae0df2632eb49ca053f91c3ce8f
Sacha <sachaobiedzy@gmail.com>
2025-10-06 16:55:03 +0200

    Feat: connection BDD

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 var cors = require('cors');
+const mysql = require('mysql2');
 
 const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+module.exports = connection;
 
 const exempleRouter = require('./route/exemple.route');
 
@@ -13,6 +22,8 @@ app.use("/exemple", exempleRouter);
 
 app.listen(port, () => {
     console.log("[API] : Ouverture du serveur...");
+    connection.connect();
+    console.log(`[API] : Serveur connecté à la base de données.`);
     console.log(`[API] : Serveur démarré sur le port ${port}.`);
 });
 
