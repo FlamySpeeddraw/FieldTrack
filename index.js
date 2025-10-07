8fd3f38c505e659f8afc0c5f9b0da65aac8afdd5
Sacha <sachaobiedzy@gmail.com>
2025-10-07 18:41:53 +0200

    Chore: changement connection mysql2 -> sequelize

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 var cors = require('cors');
-const mysql = require('mysql2/promise');
+const sequelize = require('./src/config/connexion');
 
 const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
 
-module.exports = connection;
-
-const authRouter = require('./route/auth.route');
+const authRouter = require('./src/route/auth.route');
 
 app.use("/auth", authRouter);
 
