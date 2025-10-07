46ddcd58cb8786d8bc3e374e3413cfa948b55987
Sacha <sachaobiedzy@gmail.com>
2025-10-07 18:41:24 +0200

    Chore: clean

--- route/exemple.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ExempleController = require('./../controller/exemple.controller');
-
-router.post('/exemple', ExempleController.postExemple);
-
-router.get('/exemple', ExempleController.getExemple);
-
-module.exports = router;
