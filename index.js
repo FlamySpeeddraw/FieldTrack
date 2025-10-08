34f34d83fc8f759d5dd602e8964ece7cde8c2604
Sacha <118222146+FlamySpeeddraw@users.noreply.github.com>
2025-10-08 11:11:26 +0200

    Chore: nettoyage index

--- index.js
+++ index.js
@@ -1,46 +1,30 @@
 const express = require("express");
 require("dotenv").config();
-const cors = require("cors");
-const { Sequelize, DataTypes } = require("sequelize");
+var cors = require('cors');
+const sequelize = require('./src/config/connexion');
 
+const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "mysql",
-  logging: false,
-});
-
-
-Utilisateur.belongsTo(Role, { foreignKey: "role_id", as: "role" });
-Role.hasMany(Utilisateur, { foreignKey: "role_id", as: "utilisateurs" });
-
-module.exports = { sequelize, Utilisateur, Role };
-
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("[DB] Connexion réussie à la base de données MySQL.");
-  } catch (error) {
-    console.error("[DB] Erreur de connexion :", error);
-  }
-})();
-
+const authRouter = require('./src/route/auth.route');
 const userRouter = require("./route/utilisateur.route");
+
+app.use("/auth", authRouter);
 app.use("/utilisateur", userRouter);
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`[API] Serveur démarré sur le port ${port}.`);
+    console.log("[API] : Ouverture du serveur...");
+    console.log(`[API] : Serveur démarré sur le port ${port}.`);
 });
 
-const onClose = async () => {
-  console.log("[API] Fermeture du serveur...");
-  await sequelize.close();
-  process.exit(0);
-};
+const onClose = () => {
+    console.log("[API] : Fermeture du serveur...");
+    client.close();
+}
 
-process.on("SIGINT", onClose);
-process.on("SIGTERM", onClose);
+process.on('SIGINT', onClose);
+process.on('SIGTERM', onClose);
+
+module.exports = app;
