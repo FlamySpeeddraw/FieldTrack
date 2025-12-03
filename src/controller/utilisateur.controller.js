460dc306d31cd0f490a2cc0cc490c2d0836f8c4e
Sacha <sachaobiedzy@gmail.com>
2025-12-03 12:18:02 +0100

    Fix: crash mdp

--- src/controller/utilisateur.controller.js
+++ src/controller/utilisateur.controller.js
@@ -47,7 +47,7 @@ const createUtilisateur = async (req, res, next) => {
         const roleInstance = await Role.findOne({ where: { nom_role: role } });
         if (!roleInstance) return res.status(404).json({ message: "Rôle introuvable" });
 
-        const hashedPassword = bcryptjs.hashSync(rawPassword, 10);
+        const hashedPassword = bcryptjs.hashSync(mdp, 10);
         const newUtilisateur = await User.create({ mail, mdp: hashedPassword, role_id: roleInstance.id });
         res.status(201).json({ message: "Utilisateur créé", data: newUtilisateur });
     } catch (e) { next(e); }
